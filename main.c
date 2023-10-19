b8091a66846436b9f67939595f159a8738655e40
handawoon0830 <handawoon0830@naver.com>
2023-10-19 16:04:49 +0900

    4

--- main.c
+++ main.c
@@ -2,18 +2,38 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+/*
+int i;
 
-int inc(int counter);
-int main(int argc, char *argv[]) {
-	int i=10;
-	printf("함수 호출 전 i= %d\n", i);
-	inc(i);
-	printf("함수 호출 후 i= %d \n",i);
+int main(void)
+{
+	for (i=0; i<5; i++)
+	{
+		f();
+	}
 	return 0;
 }
-
-int inc(int counter)
+void f(void)
 {
-	counter++;
-	return counter;
+	for (i=0; i<10; i++)
+		printf("#");
+}
+// f()에서 다 돌았더니 i=10이라 그게 다시 main()으로 돌아갔더니 i가 5보다 커서 바로 브레이크 됨.
+// 방법은 전엽 변수인 i를 지역변수로 바꿔주는 방법이 중요. 
+*/
+
+int main(void)
+{
+	int i; 
+	for (i=0; i<5; i++)
+	{
+		f();
+	}
+	return 0;
+}
+void f(void)
+{
+	int i;
+	for (i=0; i<10; i++)
+		printf("#");
 }
